134620b58ad754eb9b1b6d8584cf8e8f688af134
michalgalant00 <s95401@pollub.edu.pl>
2025-01-12 21:24:09 +0100

    poprawki pod kompilację

--- wordle/GuessRowView.swift
+++ wordle/GuessRowView.swift
@@ -7,36 +7,39 @@ struct GuessRowView: View {
     var body: some View {
         HStack {
             ForEach(0..<5, id: \.self) { letterIndex in
-                Flip(isFlipped: $viewModel.animationState[index * 5 + letterIndex]) {
-                    Text(viewModel.letter(at: index, letterIndex: letterIndex))
+                let isFlipped = $viewModel.animationStates.flipCards[index * 5 + letterIndex]
+                let front = {
+                    Text(viewModel.letter(at: index, column: letterIndex))
                         .frame(width: 50, height: 50)
                         .background(Color.gray)
                         .cornerRadius(8)
                         .foregroundColor(.white)
                         .font(.title)
-                } back: {
-                    Text(viewModel.letter(at: index, letterIndex: letterIndex))
+                }
+                let back = {
+                    Text(viewModel.letter(at: index, column: letterIndex))
                         .frame(width: 50, height: 50)
-                        .background(viewModel.color(at: index, letterIndex: letterIndex))
+                        .background(viewModel.color(at: index, column: letterIndex))
                         .cornerRadius(8)
                         .foregroundColor(.white)
                         .font(.title)
                 }
-                .onAppear {
-                    if index < viewModel.attempt {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + Double(letterIndex) * 0.2) {
-                            viewModel.setAnimationState(at: index * 5 + letterIndex, to: true)
+                Flip(isFlipped: isFlipped, front: front, back: back)
+                    .onAppear {
+                        if index < viewModel.attempt {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + Double(letterIndex) * 0.2) {
+                                viewModel.setAnimationState(at: index * 5 + letterIndex, to: true)
+                            }
                         }
                     }
-                }
             }
         }
-        .offset(x: viewModel.correctGuessAnimationState[index] ? 10 : 0)
+        .offset(x: viewModel.correctGuessAnimationStates[index] ? 10 : 0)
         .animation(
             .easeInOut(duration: 0.2)
                 .repeatCount(3, autoreverses: true)
                 .delay(Double(index) * 0.1),
-            value: viewModel.correctGuessAnimationState[index]
+            value: viewModel.correctGuessAnimationStates[index]
         )
     }
 }
